eec014af8a46950d3b8c834fa207c844f838459b
alnrott <alanrotta99@hotmail.com>
2024-02-06 19:12:32 -0300

    Alan/modificandoCarrousel

--- src/components/CarrouselHome.jsx
+++ src/components/CarrouselHome.jsx
@@ -1,27 +1,33 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import ImgCarrousel1 from '../Assets/images/aparts/apart1.jpeg';
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import ImgCarrousel1 from "../Assets/images/aparts/apart1.jpeg";
+import ImgCarrousel2 from "../Assets/images/mesita4.jpeg";
+import ImgCarrousel3 from "../Assets/images/mesita2.jpeg";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-  label: '1',
-  img: {ImgCarrousel1}
+    label: 1,
+    img: ImgCarrousel1,
   },
   {
-    label: '2',
-    img: {ImgCarrousel1}
-  }
+    label: 2,
+    img: ImgCarrousel2,
+  },
+  {
+    label: 3,
+    img: ImgCarrousel3,
+  },
 ];
 
 function SwipeableTextMobileStepper() {
@@ -42,22 +48,28 @@ function SwipeableTextMobileStepper() {
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+    <Box
+      sx={{
+        maxWidth: 600,
+        flexGrow: 1,
+        borderRadius: "10px",
+      }}
+    >
       <Paper
         square
         elevation={0}
         sx={{
-          display: 'flex',
-          alignItems: 'center',
+          display: "flex",
+          alignItems: "center",
           height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
+          pl: 5,
+          bgcolor: "transparent",
+          position: "absolute",
+          zIndex: 1,
         }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
+      ></Paper>
       <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
         onChangeIndex={handleStepChange}
         enableMouseEvents
@@ -68,22 +80,30 @@ function SwipeableTextMobileStepper() {
               <Box
                 component="img"
                 sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
+                  height: 400,
+                  display: "block",
+                  maxWidth: 600,
+                  overflow: "hidden",
+                  width: "100%",
+                  borderRadius: "10px",
                 }}
                 src={step.img}
-                alt={step.label}
               />
             ) : null}
+            {console.log(step)}
           </div>
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
         steps={maxSteps}
         position="static"
+        sx={{
+          position: "relative",
+          bottom: "3rem",
+          left: 0,
+          right: 0,
+          bgcolor: "transparent",
+        }}
         activeStep={activeStep}
         nextButton={
           <Button
@@ -91,8 +111,7 @@ function SwipeableTextMobileStepper() {
             onClick={handleNext}
             disabled={activeStep === maxSteps - 1}
           >
-            Next
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <KeyboardArrowLeft />
             ) : (
               <KeyboardArrowRight />
@@ -101,12 +120,11 @@ function SwipeableTextMobileStepper() {
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <KeyboardArrowRight />
             ) : (
               <KeyboardArrowLeft />
             )}
-            Back
           </Button>
         }
       />
