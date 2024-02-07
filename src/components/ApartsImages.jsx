18553fff49c5ac396e0e1a788e78d50bb1e6a92f
alnrott <alanrotta99@hotmail.com>
2024-02-07 19:18:08 -0300

    Solucionando importacion de imagenes

--- src/components/ApartsImages.jsx
+++ src/components/ApartsImages.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const ApartsImages = ({ images }) => {
   return (
-    <Box sx={{ width: '100%', height: 450, overflowY: 'scroll' }}>
+    <Box sx={{ width: "100%", height: 450, overflowY: "scroll" }}>
       <ImageList variant="masonry" cols={3} gap={8}>
-        {images.map((image, index) => (
+        {images[0].map((image, index) => (
           <ImageListItem key={index}>
-            <img
-              srcSet={`${image}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              src={`${image}?w=248&fit=crop&auto=format`}
-              alt={`Apart image ${index}`}
-              loading="lazy"
-            />
+            <img src={image} alt={`Image ${index}`} loading="lazy" />
           </ImageListItem>
         ))}
       </ImageList>
