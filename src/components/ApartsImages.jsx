9ce51905bfa109b0f5cc8659ba888ad6994e290b
Tomas Giacobino <tgiacobino@pgmtools.com.ar>
2024-02-13 22:52:06 -0300

    estilos y responsive

--- src/components/ApartsImages.jsx
+++ src/components/ApartsImages.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
